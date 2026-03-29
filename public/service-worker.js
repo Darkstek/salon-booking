c3c696c5350861ecb3dbedf74f63d0b0f4c2572a
Darkstek <Darkstek@gmail.com>
2026-03-29 12:26:37 +0200

    Fix service worker auto-update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,24 @@
 const CACHE_NAME = 'salon-booking-v1';
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/nightcity.jpg',
-      ]);
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
