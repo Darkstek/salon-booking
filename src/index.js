f240921ceeca7aadc175a0ff0c5475ada5b7989f
Darkstek <Darkstek@gmail.com>
2026-04-04 17:36:14 +0200

    index.tsx zpět na index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import "./index.css";
 import App from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <GoogleOAuthProvider clientId="783420070926-3fijlp9s34nlo0q8joh04ughnn707ml2.apps.googleusercontent.com">
     <App />
